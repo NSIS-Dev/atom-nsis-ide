6febd6269b1e5e85f56a91c8c201b661c35bc992
idleberg <jan@idleberg.com>
2016-12-07 12:48:58 +0100

    use Gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 // Dependencies
-const gulp = require('gulp');
-const debug = require('gulp-debug');
 const coffeelint = require('gulp-coffeelint');
+const debug = require('gulp-debug');
+const gulp = require('gulp');
 const jshint = require('gulp-jshint');
 const jsonlint = require('gulp-jsonlint');
 const lesshint = require('gulp-lesshint');
+const stylish = require('coffeelint-stylish');
 
 // Files
 const coffeeFiles = [
@@ -31,36 +32,45 @@ const lessFiles = [
     './styles/**/*.less'
 ];
 
-// Tasks
-gulp.task('lint', ['coffeelint', 'jshint', 'jsonlint', 'lesshint']);
-
 // Lint CoffeeScript & CSON files
-gulp.task('coffeelint', function () {
+gulp.task('lint:coffee', gulp.series(function(done) { 
     gulp.src(coffeeFiles)
         .pipe(debug({title: 'coffeelint:'}))
         .pipe(coffeelint())
-        .pipe(coffeelint.reporter('fail'))
-});
+        .pipe(coffeelint.reporter(stylish))
+        .pipe(coffeelint.reporter('fail'));
+    done();
+}));
 
 // Lint LESS files
-gulp.task('lesshint', function () {
-     gulp.src(lessFiles)
+gulp.task('lint:less', gulp.series(function(done) { 
+    gulp.src(lessFiles)
         .pipe(debug({title: 'lesshint:'}))
         .pipe(lesshint())
         .pipe(lesshint.reporter());
-});
+    done();
+}));
 
 // Lint JavaScript files
-gulp.task('jshint', function() {
-    return gulp.src(jsFiles)
+gulp.task('lint:js', gulp.series(function(done) { 
+    gulp.src(jsFiles)
         .pipe(debug({title: 'jshint:'}))
         .pipe(jshint());
-});
+    done();
+}));
 
 // Lint JSON files
-gulp.task('jsonlint', function() {
-   gulp.src(jsonFiles)
+gulp.task('lint:json', gulp.series(function(done) { 
+    gulp.src(jsonFiles)
         .pipe(debug({title: 'jsonlint:'}))
         .pipe(jsonlint())
+        .pipe(jsonlint.failAfterError())
         .pipe(jsonlint.reporter());
-});
+    done();
+}));
+
+// Tasks
+gulp.task('lint', gulp.parallel('lint:coffee', 'lint:js', 'lint:json', 'lint:less', function(done) {
+  done();
+}));
+
